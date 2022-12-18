ce2a03063099de305b00d2bddac72b4d074c2b9b
Akhil <56110114+AKHILKRISHNA-CPU@users.noreply.github.com>
2022-12-18 17:01:46 +0530

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,17 @@
+# Created two Pandas series
+FirstSeries = pd.Series([10,15,20,25,30,35])
+SecondSeries = pd.Series([2,4,6,8,10,12])
+# Performing arithmetic operations on the two series
+addition = FirstSeries + SecondSeries
+subtraction = FirstSeries - SecondSeries
+multiplication = FirstSeries * SecondSeries
+division = FirstSeries / SecondSeries
+modulus = FirstSeries % SecondSeries
+# Printing the results of the arithmetic operations
+print(addition)
+print(subtraction)
+print(multiplication)
+print(division)
+print(modulus)
+
+
